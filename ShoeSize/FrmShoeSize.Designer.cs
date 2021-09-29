09f08548a3f97e9f08275f6df1aabe28bb649008
Irtaza Chohan <irtazachohan@gmail.com>
2021-09-29 15:39:27 +0100

    Add project files.

---
+++ ShoeSize/FrmShoeSize.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace ShoeSize
+{
+    partial class FrmShoeSize
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnShoeSize = new System.Windows.Forms.Button();
+            this.txtMessageBox = new System.Windows.Forms.TextBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // BtnShoeSize
+            // 
+            this.BtnShoeSize.Location = new System.Drawing.Point(196, 24);
+            this.BtnShoeSize.Name = "BtnShoeSize";
+            this.BtnShoeSize.Size = new System.Drawing.Size(147, 40);
+            this.BtnShoeSize.TabIndex = 0;
+            this.BtnShoeSize.Text = "Calculate Price";
+            this.BtnShoeSize.UseVisualStyleBackColor = true;
+            this.BtnShoeSize.Click += new System.EventHandler(this.BtnShoeSize_Click);
+            // 
+            // txtMessageBox
+            // 
+            this.txtMessageBox.Location = new System.Drawing.Point(68, 95);
+            this.txtMessageBox.Multiline = true;
+            this.txtMessageBox.Name = "txtMessageBox";
+            this.txtMessageBox.Size = new System.Drawing.Size(410, 62);
+            this.txtMessageBox.TabIndex = 1;
+            this.txtMessageBox.Text = "Please select Size of Shoe and how many needed";
+            this.txtMessageBox.TextChanged += new System.EventHandler(this.txtMessageBox_TextChanged);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numericUpDown1.Location = new System.Drawing.Point(374, 24);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            15,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(104, 40);
+            this.numericUpDown1.TabIndex = 2;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(348, 67);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(130, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Enter no. of shoes desired";
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(331, 256);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(147, 39);
+            this.btnExit.TabIndex = 4;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(65, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Enter shoe size";
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.DecimalPlaces = 1;
+            this.numericUpDown2.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numericUpDown2.Increment = new decimal(new int[] {
+            5,
+            0,
+            0,
+            65536});
+            this.numericUpDown2.Location = new System.Drawing.Point(68, 24);
+            this.numericUpDown2.Maximum = new decimal(new int[] {
+            15,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(104, 40);
+            this.numericUpDown2.TabIndex = 5;
+            this.numericUpDown2.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // FrmShoeSize
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(530, 318);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.txtMessageBox);
+            this.Controls.Add(this.BtnShoeSize);
+            this.Name = "FrmShoeSize";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Shoe Size Selector";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnShoeSize;
+        private System.Windows.Forms.TextBox txtMessageBox;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+    }
+}
+
---
+++ ShoeSize/FrmShoeSize.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ShoeSize
+{
+    public partial class FrmShoeSize : Form
+    {
+        public FrmShoeSize()
+        {
+            InitializeComponent();
+        }
+
+        int shoesize;
+        double price;
+        int numberofshoes;
+        double total=0;
+
+        private void BtnShoeSize_Click(object sender, EventArgs e)
+        {
+            // shoesize = int.Parse(Microsoft.VisualBasic.Interaction.InputBox("Please enter shoe size", "Shoe Size Entry Box", ""));
+
+            shoesize = (int)numericUpDown2.Value;
+            numberofshoes = (int)numericUpDown1.Value;
+
+            if (shoesize <= 4)
+            {
+                price = 5.00;
+                total = (price * numberofshoes);
+                txtMessageBox.Text = "Head over to the Children Section and price will be " + total.ToString("c");
+            }
+            else if (shoesize >= 5 && shoesize <= 9){
+                price = 10.00;
+                total = (price * numberofshoes);
+                txtMessageBox.Text = "Head over to Teenagers Section and price will be " + total.ToString("c");
+            }
+            else if (shoesize >= 9)
+            {
+                price = 15.00;
+                total = (price * numberofshoes);
+                txtMessageBox.Text = "Head over to the Adults section and price will be " + total.ToString("c");
+            }
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void txtMessageBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
